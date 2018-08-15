caedcd275be542e5ecaf296fc48f50641f249f48
laoxu <xsmlxh666@163.com>
2018-08-15 22:18:06 +0800

    model  函数封装

--- App/models/ModelUtil.py
+++ App/models/ModelUtil.py
@@ -14,3 +14,13 @@ class BaseModel:
         except Exception as e:
             print("error:", e)
             return False
+
+    # save 创建保存数据方法
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except Exception as e:
+            print("error:", e)
+            return False
