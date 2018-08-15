07b609bb719f7cabd54fc723b87bd4550ef9e580
laoxu <xsmlxh666@163.com>
2018-08-15 20:03:31 +0800

    封装 函数    所有模型基础自 BaseModel

---
+++ App/models/ModelUtil.py
@@ -0,0 +1,16 @@
+from flask_restful import abort
+
+from App.ext import db
+
+
+class BaseModel:
+
+    # save 创建保存数据方法
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except Exception as e:
+            print("error:", e)
+            return False
