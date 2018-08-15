7860dbc3a9ca471d7bf8085b864863d0cdaf6244
laoxu <xsmlxh666@163.com>
2018-08-15 19:38:12 +0800

    HelloApi  调试

--- App/apis/HelloApi.py
+++ App/apis/HelloApi.py
@@ -3,4 +3,13 @@ from flask_restful import Resource
 
 class Hello(Resource):
     def get(self):
-        return {"msg": 'ok'}
+        return {"msg": 'Hello get'}
+
+    def post(self):
+        return {"msg": 'Hello post'}
+
+    def delete(self):
+        return {"msg": 'Hello delete'}
+
+    def put(self):
+        return {"msg": 'Hello put'}
