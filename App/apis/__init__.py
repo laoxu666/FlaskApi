edc3cbe6121459fecfe80a197185460aceec82f1
laoxu <xsmlxh666@163.com>
2018-08-19 20:57:13 +0800

    api.add_resource 各种源

--- App/apis/__init__.py
+++ App/apis/__init__.py
@@ -1,8 +1,11 @@
 from flask_restful import Api
 
 
+from App.apis.BookApi import BookResource
+from App.apis.CodeApi import CodeResource
 from App.apis.HelloApi import Hello
 from App.apis.MovieApi import MovieResource, MoviesResource
+from App.apis.UserApi import UsersResource, UserResource
 
 api = Api()
 
@@ -14,3 +17,7 @@ def init_api(app):
 api.add_resource(Hello, '/hello/')
 api.add_resource(MoviesResource, '/movies/')
 api.add_resource(MovieResource, '/movies/<int:id>/')
+api.add_resource(UsersResource, "/users/")
+api.add_resource(UserResource, "/user/<int:id>/")
+api.add_resource(BookResource,"/books/")
+api.add_resource(CodeResource,"/codes/")
